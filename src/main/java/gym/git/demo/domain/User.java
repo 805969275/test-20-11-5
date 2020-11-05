74c8218a26d8808e096db0fc3f416cf28741ea27
805969275 <805969275@qq.com>
2020-11-05 16:53:00 +0800

    测试推送111

---
+++ src/main/java/gym/git/demo/domain/User.java
@@ -0,0 +1,12 @@
+package gym.git.demo.domain;
+
+/**
+ * @ClassName : User  //类名
+ * @Description :   //描述
+ * @Author : GYM->郭永明  //作者
+ * @Date: 2020-11-05 16:51  //时间
+ */
+public class User {
+    private String name;
+    private Integer age;
+}
